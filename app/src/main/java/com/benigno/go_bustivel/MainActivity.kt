82e88f5c9857c4ba92482c9b27b3de8a82c2a23f
Maurício Benigno <mauriciobenigno@hotmail.com>
2019-07-31 19:12:45 -0300

    Initial commit

---
+++ app/src/main/java/com/benigno/go_bustivel/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.benigno.go_bustivel
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
