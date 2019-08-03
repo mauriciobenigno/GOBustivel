688f9ae87ea88429ebe116f9a8098a4d9e16ba50
Maurício Benigno <mauriciobenigno@hotmail.com>
2019-08-03 18:55:16 -0300

    #3# Adição parcial de tela de cadastro para teste de inserção de dados no banco de dados.

--- app/src/main/java/com/benigno/go_bustivel/MainActivity.kt
+++ app/src/main/java/com/benigno/go_bustivel/MainActivity.kt
@@ -1,12 +1,42 @@
 package com.benigno.go_bustivel
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.benigno.go_bustivel.TelasTeste.TelaCadastroPosto
+import com.benigno.go_bustivel.TelasTeste.TelaConsultaPosto
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // BOTÕES
+        var btCadastro = findViewById<Button>(R.id.btCadastro)
+        var btExcluir = findViewById<Button>(R.id.btExcluir)
+        var btConsultar = findViewById<Button>(R.id.btConsulta)
+
+
+        btCadastro.setOnClickListener {
+            val intentTelaCadastroPosto = Intent(this, TelaCadastroPosto::class.java)
+            startActivity(intentTelaCadastroPosto)
+        }
+
+        btExcluir.setOnClickListener {
+            val intentTelaExcluirPosto = Intent(this, TelaConsultaPosto::class.java)
+            startActivity(intentTelaExcluirPosto)
+        }
+
+        btConsultar.setOnClickListener {
+            val intentTelaConsultaPosto = Intent(this, TelaConsultaPosto::class.java)
+            startActivity(intentTelaConsultaPosto)
+        }
     }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/benigno/go_bustivel/TelasTeste/TelaConsultaPosto.kt
@@ -0,0 +1,13 @@
+package com.benigno.go_bustivel.TelasTeste
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.benigno.go_bustivel.R
+
+class TelaConsultaPosto : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tela_consulta_posto)
+    }
+}
